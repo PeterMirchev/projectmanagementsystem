c88a46f0efc15bcf35f880232e369ff7dd562f8a
Peter <guitaronline89@gmail.com>
2024-08-20 15:43:16 +0300

    Implementing Jwt

--- src/main/java/com/fixer/config/JwtConstants.java
+++ src/main/java/com/fixer/config/JwtConstants.java
@@ -2,7 +2,7 @@ package com.fixer.config;
 
 public class JwtConstants {
 
-    public static final String SECRET_Key = "asdasdsadgfsdgs 325 etw  *&%&$^&898 98686 6 dgs ddaf hadfh ad h";
+    public static final String SECRET_KEY = "asdasdsadgfsdgs 325 etw  *&%&$^&898 98686 6 dgs ddaf hadfh ad h";
     public static final String JWT_HEADER = "Authorization";
 
 }
